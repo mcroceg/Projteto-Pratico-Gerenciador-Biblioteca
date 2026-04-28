295d0c0093c36eb7d8a149b0e430801f54e346ad
matheus croce <mcrocegarcia@gmail.com>
2026-04-28 00:40:57 -0300

    criado a estrutura básica do projeto com arquivos de informação sobre os livros e os arrays para dar push no código principal do index.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const titulos: string[] = [ "O Hobit", "Clean Code", "1984", "Dom Casmurro", "O Nome do Vento"];
+const autores: string[] = [ "J.R.R. Tolkien", "Robert C. Martin", "George Orwell", "Machado de Assis", "Patrick Rothfuss"];
+const anos: number[] = [1937, 2008, 1949, 1899, 2007];
+const paginas: number[] = [310, 464, 328, 256, 662];
+const lidos: boolean[] = [true, false, true, false, true];
+const avaliacoes: number[] = [4.5, 5.0, 4.0, 3.5, 4.8]; // 0 se não lido, 1 a 5 se lido
